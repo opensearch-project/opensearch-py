ef88f43a72f2970d6e6fe4359366b76db7afa52a
Rushi Agrawal <rushi.agr@gmail.com>
2021-09-17 02:21:57 +0530

    Update package name to opensearch-py (#72)
    Since opensearch is already taken, update distribution name to opensearch-py.
    Update build utils accordingly to fix package name change.
    
    Signed-off-by: Rushi Agrawal <rushi.agr@gmail.com>

--- setup.py
+++ setup.py
@@ -30,10 +30,10 @@ from os.path import abspath, dirname, join
 
 from setuptools import find_packages, setup
 
-package_name = "opensearchpy"
+package_name = "opensearch-py"
 base_dir = abspath(dirname(__file__))
 
-with open(join(base_dir, package_name, "_version.py")) as f:
+with open(join(base_dir, package_name.replace("-", ""), "_version.py")) as f:
     package_version = re.search(
         r"__versionstr__\s+=\s+[\"\']([^\"\']+)[\"\']", f.read()
     ).group(1)
