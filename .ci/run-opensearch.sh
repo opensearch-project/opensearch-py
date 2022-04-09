ad9d5dc3061f1374cfc56e6efe593d15495130a2
Vacha Shah <vachshah@amazon.com>
2022-04-09 00:10:37 -0700

    Adding support to test against unreleased OpenSearch
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

--- .ci/run-opensearch.sh
+++ .ci/run-opensearch.sh
@@ -30,7 +30,7 @@ environment=($(cat <<-END
 END
 ))
 
-if [[ "$SECURE_INTEGRATION" == "false" ]] && [[ "$CLUSTER" == "opensearch" ]]; then
+if [[ "$SECURE_INTEGRATION" == "false" ]] && [[ "$CLUSTER" == "opensearch" ]] && [[ "$IS_UNRELEASED" == "false" ]]; then
   security=($(cat <<-END
     --env plugins.security.disabled=true
 END
@@ -58,40 +58,62 @@ END
   local_detach="true"
   if [[ "$i" == "$((NUMBER_OF_NODES-1))" ]]; then local_detach=$DETACH; fi
 
-  echo -e "\033[34;1mINFO: building $CLUSTER container\033[0m"
-  docker build \
-    --file=.ci/$CLUSTER/Dockerfile \
-    --build-arg SECURE_INTEGRATION=$SECURE_INTEGRATION \
-    --build-arg OPENSEARCH_VERSION=$OPENSEARCH_VERSION \
-    --tag=$CLUSTER-secure-$SECURE_INTEGRATION \
-    .
-
-  echo -e "\033[34;1mINFO:\033[0m Starting container $node_name \033[0m"
   set -x
   healthcmd="curl -vvv -s --fail http://localhost:9200/_cluster/health || exit 1"
   if [[ "$SECURE_INTEGRATION" == "true" ]]; then
     healthcmd="curl -vvv -s --insecure -u admin:admin --fail https://localhost:9200/_cluster/health || exit 1"
   fi
 
-  docker run \
-    --name "$node_name" \
-    --network "$network_name" \
-    --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
-    "${environment[@]}" \
-    "${volumes[@]}" \
-    "${security[@]}" \
-    --publish "$http_port":9200 \
-    --ulimit nofile=65536:65536 \
-    --ulimit memlock=-1:-1 \
-    --detach="$local_detach" \
-    --health-cmd="$(echo $healthcmd)" \
-    --health-interval=2s \
-    --health-retries=20 \
-    --health-timeout=2s \
-    --rm \
-    -d \
-    $CLUSTER-secure-$SECURE_INTEGRATION;
+  if [[ "$IS_UNRELEASED" == "false" ]]; then
+    echo -e "\033[34;1mINFO: building $CLUSTER container\033[0m"
+    docker build \
+      --file=.ci/$CLUSTER/Dockerfile \
+      --build-arg SECURE_INTEGRATION=$SECURE_INTEGRATION \
+      --build-arg OPENSEARCH_VERSION=$OPENSEARCH_VERSION \
+      --tag=$CLUSTER-secure-$SECURE_INTEGRATION \
+      .
 
+    echo -e "\033[34;1mINFO:\033[0m Starting container $node_name \033[0m"
+
+    docker run \
+      --name "$node_name" \
+      --network "$network_name" \
+      --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+      "${environment[@]}" \
+      "${volumes[@]}" \
+      "${security[@]}" \
+      --publish "$http_port":9200 \
+      --ulimit nofile=65536:65536 \
+      --ulimit memlock=-1:-1 \
+      --detach="$local_detach" \
+      --health-cmd="$(echo $healthcmd)" \
+      --health-interval=2s \
+      --health-retries=20 \
+      --health-timeout=2s \
+      --rm \
+      -d \
+      $CLUSTER-secure-$SECURE_INTEGRATION;
+  else
+    docker run \
+      --name "$node_name" \
+      --network "$network_name" \
+      --env "ES_JAVA_OPTS=-Xms1g -Xmx1g" \
+      "${environment[@]}" \
+      "${volumes[@]}" \
+      "${security[@]}" \
+      --publish "$http_port":9200 \
+      --ulimit nofile=65536:65536 \
+      --ulimit memlock=-1:-1 \
+      --detach="$local_detach" \
+      --health-cmd="$(echo $healthcmd)" \
+      --health-interval=2s \
+      --health-retries=20 \
+      --health-timeout=2s \
+      --rm \
+      -d \
+      opensearch:test;
+  fi
+  
   set +x
   if wait_for_container "$opensearch_node_name" "$network_name"; then
     echo -e "\033[32;1mSUCCESS:\033[0m Running on: $node_url\033[0m"
