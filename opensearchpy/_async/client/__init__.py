20979d1fc5dcd43700f8b63473392351dfd464b1
amoghtewari <amoghtewari@users.noreply.github.com>
2022-09-06 17:52:52 +0530

    Update __init__.py (Inclusive naming)

--- opensearchpy/_async/client/__init__.py
+++ opensearchpy/_async/client/__init__.py
@@ -688,14 +688,14 @@ class AsyncOpenSearch(object):
             headers=headers,
         )
 
-    @query_params("master_timeout", "timeout")
+    @query_params("leader_timeout", "timeout")
     async def delete_script(self, id, params=None, headers=None):
         """
         Deletes a script.
 
 
         :arg id: Script ID
-        :arg master_timeout: Specify timeout for connection to master
+        :arg leader_timeout: Specify timeout for connection to leader
         :arg timeout: Explicit operation timeout
         """
         if id in SKIP_IN_PATH:
@@ -936,14 +936,14 @@ class AsyncOpenSearch(object):
             "GET", _make_path(index, doc_type, id), params=params, headers=headers
         )
 
-    @query_params("master_timeout")
+    @query_params("leader_timeout")
     async def get_script(self, id, params=None, headers=None):
         """
         Returns a script.
 
 
         :arg id: Script ID
-        :arg master_timeout: Specify timeout for connection to master
+        :arg leader_timeout: Specify timeout for connection to leader
         """
         if id in SKIP_IN_PATH:
             raise ValueError("Empty value passed for a required argument 'id'.")
@@ -1197,7 +1197,7 @@ class AsyncOpenSearch(object):
             "POST", path, params=params, headers=headers, body=body
         )
 
-    @query_params("master_timeout", "timeout")
+    @query_params("leader_timeout", "timeout")
     async def put_script(self, id, body, context=None, params=None, headers=None):
         """
         Creates or updates a script.
@@ -1206,7 +1206,7 @@ class AsyncOpenSearch(object):
         :arg id: Script ID
         :arg body: The document
         :arg context: Context name to compile script against
-        :arg master_timeout: Specify timeout for connection to master
+        :arg leader_timeout: Specify timeout for connection to leader
         :arg timeout: Explicit operation timeout
         """
         for param in (id, body):
@@ -1576,7 +1576,7 @@ class AsyncOpenSearch(object):
         :arg ignore_unavailable: Whether specified concrete indices
             should be ignored when unavailable (missing or closed)
         :arg local: Return local information, do not retrieve the state
-            from master node (default: false)
+            from leader node (default: false)
         :arg preference: Specify the node or shard the operation should
             be performed on (default: random)
         :arg routing: Specific routing value
@@ -2012,3 +2012,4 @@ class AsyncOpenSearch(object):
             headers=headers,
             body=body,
         )
+    
