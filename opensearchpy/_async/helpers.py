e5d23f6bfbdfd35e7d2115f5bc776ce7496e57db
Arnav Das <arnav.das88@gmail.com>
2022-09-07 11:58:51 +0000

    License Headers fixed
    Signed-off-by: Arnav Das <arnav.das88@gmail.com>

--- opensearchpy/_async/helpers.py
+++ opensearchpy/_async/helpers.py
@@ -6,23 +6,7 @@
 #
 # Modifications Copyright OpenSearch Contributors. See
 # GitHub history for details.
-#
-#  Licensed to Elasticsearch B.V. under one or more contributor
-#  license agreements. See the NOTICE file distributed with
-#  this work for additional information regarding copyright
-#  ownership. Elasticsearch B.V. licenses this file to you under
-#  the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-# 	http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing,
-#  software distributed under the License is distributed on an
-#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-#  KIND, either express or implied.  See the License for the
-#  specific language governing permissions and limitations
-#  under the License.
+
 
 # Licensed to Elasticsearch B.V.under one or more agreements.
 # Elasticsearch B.V.licenses this file to you under the Apache 2.0 License.
@@ -203,7 +187,7 @@ async def async_streaming_bulk(
                         raise_on_error,
                         ignore_status,
                         *args,
-                        **kwargs,
+                        **kwargs
                     ),
                 ):
 
@@ -470,5 +454,5 @@ async def async_reindex(
         target_client,
         _change_doc_index(docs, target_index),
         chunk_size=chunk_size,
-        **kwargs,
+        **kwargs
     )
