79fa2d33ff46f492508a4a35827be158f16a0d0c
Harsha Vamsi Kalluri <harshavamsi096@gmail.com>
2023-01-17 16:54:02 +0000

    Remove requirement for x-amz-content-sha256 header
    Signed-off-by: Harsha Vamsi Kalluri <harshavamsi096@gmail.com>

--- opensearchpy/helpers/asyncsigner.py
+++ opensearchpy/helpers/asyncsigner.py
@@ -9,8 +9,6 @@
 
 import sys
 
-from six import text_type
-
 PY3 = sys.version_info[0] == 3
 
 
@@ -41,7 +39,6 @@ class AWSV4SignerAsyncAuth:
         :param prepared_request: unsigned headers
         :return: signed headers
         """
-        import hashlib
 
         from botocore.auth import SigV4Auth
         from botocore.awsrequest import AWSRequest
@@ -50,20 +47,7 @@ class AWSV4SignerAsyncAuth:
         aws_request = AWSRequest(
             method=method,
             url="".join([url, query_string]),
-            data=body,
         )
-
-        if aws_request.body is not None:
-            if hasattr(aws_request.body, "read"):
-                aws_request.body = aws_request.body.read()
-            self.encode_body(aws_request)
-            content_hash = hashlib.sha256(aws_request.body)
-        elif hasattr(aws_request, "content") and aws_request.content is not None:
-            content_hash = hashlib.sha256(aws_request.content)
-        else:
-            content_hash = hashlib.sha256(b"")
-
-        aws_request.headers["x-amz-content-sha256"] = content_hash.hexdigest()
 
         sig_v4_auth = SigV4Auth(self.credentials, self.service, self.region)
         sig_v4_auth.add_auth(aws_request)
@@ -73,20 +57,3 @@ class AWSV4SignerAsyncAuth:
 
         # copy the headers from AWS request object into the prepared_request
         return dict(aws_request.headers.items())
-
-    # inspired by https://github.com/tedder/requests-aws4auth
-    @staticmethod
-    def encode_body(req):
-        if isinstance(req.body, text_type):
-            split = req.headers.get("content-type", "text/plain").split(";")
-            if len(split) == 2:
-                ct, cs = split
-                cs = cs.split("=")[1]
-                req.body = req.body.encode(cs)
-            else:
-                ct = split[0]
-                if ct == "application/x-www-form-urlencoded" or "x-amz-" in ct:
-                    req.body = req.body.encode()
-                else:
-                    req.body = req.body.encode("utf-8")
-                    req.headers["content-type"] = ct + "; charset=utf-8"
--- opensearchpy/helpers/signer.py
+++ opensearchpy/helpers/signer.py
@@ -10,7 +10,6 @@
 import sys
 
 import requests
-from six import text_type
 
 PY3 = sys.version_info[0] == 3
 
@@ -72,8 +71,6 @@ class AWSV4SignerAuth(requests.auth.AuthBase):
         :return: signed request
         """
 
-        import hashlib
-
         from botocore.auth import SigV4Auth
         from botocore.awsrequest import AWSRequest
 
@@ -83,23 +80,7 @@ class AWSV4SignerAuth(requests.auth.AuthBase):
         aws_request = AWSRequest(
             method=prepared_request.method.upper(),
             url=url,
-            data=prepared_request.body,
         )
-
-        if hasattr(prepared_request, "body") and prepared_request.body is not None:
-            if hasattr(prepared_request.body, "read"):
-                prepared_request.body = prepared_request.body.read()
-            self.encode_body(prepared_request)  # type: ignore
-            content_hash = hashlib.sha256(prepared_request.body)
-        elif (
-            hasattr(prepared_request, "content")
-            and prepared_request.content is not None
-        ):
-            content_hash = hashlib.sha256(prepared_request.content)
-        else:
-            content_hash = hashlib.sha256(b"")
-
-        prepared_request.headers["x-amz-content-sha256"] = content_hash.hexdigest()
 
         sig_v4_auth = SigV4Auth(self.credentials, self.service, self.region)
         sig_v4_auth.add_auth(aws_request)
@@ -111,20 +92,3 @@ class AWSV4SignerAuth(requests.auth.AuthBase):
             del prepared_request.headers["Content-Length"]
 
         return prepared_request
-
-    # inspired by https://github.com/tedder/requests-aws4auth
-    @staticmethod
-    def encode_body(req):  # type: ignore
-        if isinstance(req.body, text_type):
-            split = req.headers.get("content-type", "text/plain").split(";")
-            if len(split) == 2:
-                ct, cs = split
-                cs = cs.split("=")[1]
-                req.body = req.body.encode(cs)
-            else:
-                ct = split[0]
-                if ct == "application/x-www-form-urlencoded" or "x-amz-" in ct:
-                    req.body = req.body.encode()
-                else:
-                    req.body = req.body.encode("utf-8")
-                    req.headers["content-type"] = ct + "; charset=utf-8"
