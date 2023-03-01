5987cf25ac4713a5f0b6632993456d014935df87
Sai Medhini Reddy Maryada <117196660+saimedhi@users.noreply.github.com>
2023-02-28 18:19:57 -0800

    Update test_search.py

--- test_opensearchpy/test_async/test_server/test_helpers/test_search.py
+++ test_opensearchpy/test_async/test_server/test_helpers/test_search.py
@@ -174,7 +174,12 @@ async def test_raw_subfield_can_be_used_in_aggs(data_client):
     s.aggs.bucket("authors", "terms", field="author.name.raw", size=1)
 
     r = await s.execute()
-
+    print("Remove later1")
+    print(r)
+    print("Remove later2")
     authors = r.aggregations.authors
+    print("Remove later3")
+    print(authors)
+    print("Remove later4")
     assert 1 == len(authors)
     assert {"key": "Honza Král", "doc_count": 52} == authors[0]
