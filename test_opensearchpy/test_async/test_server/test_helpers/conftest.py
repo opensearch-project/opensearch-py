1d907bdf6a4e283324439f64c2027cd97c59d5d8
saimedhi <saimedhi@amazon.com>
2023-03-03 11:36:31 -0800

    Removing print Remove later
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- test_opensearchpy/test_async/test_server/test_helpers/conftest.py
+++ test_opensearchpy/test_async/test_server/test_helpers/conftest.py
@@ -57,39 +57,6 @@ def event_loop():
     yield loop
     loop.close()
 
-# @fixture(scope="session")
-# async def client():
-#     opensearch_url = "http://localhost:9200/"
-#     kwargs = {}
-#     # "verify_certs": False will optionally generate a warning message
-#     # (see :class:`~opensearchpy.Urllib3HttpConnection` for detailed description of the options)::
-#     if (
-#         "SECURE_INTEGRATION" in os.environ
-#         and os.environ["SECURE_INTEGRATION"] == "true"
-#     ):
-#         opensearch_url = "https://localhost:9200/"
-#         kwargs = {
-#             "http_auth": ("admin", "admin"),
-#             "verify_certs": False,
-#         }
-#     try:
-#         print("Remove Later0")
-#         client = AsyncOpenSearch(opensearch_url, **kwargs)
-#         print("Remove Later4")
-#         await client.cluster.health(wait_for_status="yellow")
-#         print("Remove Later5")
-#         await add_connection("default", client)
-#         print("Remove Later6")
-#         return client
-#         print("Remove Later7")
-#     except ConnectionError as e:
-#         print("Remove Later1")
-#         print(e.__str__())
-#         print("Remove Later2")
-#         print(e)
-#         print("Remove Later3")
-#     #except ConnectionError:
-#         #skip()
 
 
 @fixture(scope="session")
