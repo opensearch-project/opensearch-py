f3e9be74e675e08c246e8ee8781121cb0884000c
bl1nkker <nurovich14@gmail.com>
2023-03-16 18:21:27 +0600

    fix: fixed test coverage statistics on codecov
    Signed-off-by: Daniyar Auezkhan <nurovich@google.com>

--- test_opensearchpy/run_tests.py
+++ test_opensearchpy/run_tests.py
@@ -91,16 +91,21 @@ def run_all(argv=None):
 
     # always insert coverage when running tests
     if argv is None:
+
         junit_xml = join(
             abspath(dirname(dirname(__file__))), "junit", "opensearch-py-junit.xml"
         )
+        codecov_xml = join(
+            abspath(dirname(dirname(__file__))), "junit", "opensearch-py-codecov.xml"
+        )
         argv = [
             "pytest",
-            "--cov=opensearch",
+            "--cov=opensearchpy",
             "--junitxml=%s" % junit_xml,
             "--log-level=DEBUG",
             "--cache-clear",
             "-vv",
+            "--cov-report=xml:%s" % codecov_xml,
         ]
 
         secured = False
