b33ab974af2aa218ba03bdd915be312bae54c4ac
Temirlan-qa <temirlanaazbek@gmail.com>
2023-04-19 12:03:23 +0600

    trust_env to the aiohttp.ClientSession
    Signed-off-by: Temirlan-qa <temirlanaazbek@gmail.com>

--- opensearchpy/_async/http_aiohttp.py
+++ opensearchpy/_async/http_aiohttp.py
@@ -91,6 +91,7 @@ class AIOHttpConnection(AsyncConnection):
         http_compress=None,
         opaque_id=None,
         loop=None,
+        trust_env=False,
         **kwargs
     ):
         """
@@ -219,6 +220,7 @@ class AIOHttpConnection(AsyncConnection):
         self._limit = maxsize
         self._http_auth = http_auth
         self._ssl_context = ssl_context
+        self._trust_env = trust_env
 
     async def perform_request(
         self, method, url, params=None, body=None, timeout=None, ignore=(), headers=None
@@ -354,6 +356,7 @@ class AIOHttpConnection(AsyncConnection):
         if self.loop is None:
             self.loop = get_running_loop()
         self.session = aiohttp.ClientSession(
+            trust_env=self._trust_env,
             headers=self.headers,
             skip_auto_headers=("accept", "accept-encoding"),
             auto_decompress=True,
