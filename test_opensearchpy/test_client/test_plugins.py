6194c569967ed1c55384794b381c1a6a4366f151
florian <florian@harfanglab.fr>
2023-05-25 13:00:51 +0200

    feat(plugins): add index management client plugin
    Signed-off-by: florian <florian@harfanglab.fr>

--- test_opensearchpy/test_client/test_plugins.py
+++ test_opensearchpy/test_client/test_plugins.py
@@ -75,3 +75,45 @@ class TestAlerting(OpenSearchTestCase):
         self.assert_url_called(
             "POST", "/_plugins/_alerting/monitors/.../_acknowledge/alerts"
         )
+
+
+class TestIndexManagement(OpenSearchTestCase):
+    def test_create_policy(self):
+        self.client.index_management.put_policy("...")
+        self.assert_url_called("PUT", "/_plugins/_ism/policies/...")
+
+    def test_update_policy(self):
+        self.client.index_management.put_policy(
+            "...", params={"if_seq_no": 7, "if_primary_term": 1}
+        )
+        self.assert_url_called(
+            "PUT", "/_plugins/_ism/policies/...?if_seq_no=7&if_primary_term=1"
+        )
+
+    def test_add_policy(self):
+        self.client.index_management.add_policy("...")
+        self.assert_url_called("POST", "/_plugins/_ism/add/...")
+
+    def test_get_policy(self):
+        self.client.index_management.get_policy("...")
+        self.assert_url_called("GET", "/_plugins/_ism/policies/...")
+
+    def test_remove_policy_from_index(self):
+        self.client.index_management.remove_policy_from_index("...")
+        self.assert_url_called("POST", "/_plugins/_ism/remove/...")
+
+    def test_change_policy(self):
+        self.client.index_management.change_policy("...")
+        self.assert_url_called("POST", "/_plugins/_ism/change_policy/...")
+
+    def test_retry(self):
+        self.client.index_management.retry("...")
+        self.assert_url_called("POST", "/_plugins/_ism/retry/...")
+
+    def test_explain_index(self):
+        self.client.index_management.explain_index("...", show_policy=True)
+        self.assert_url_called("GET", "/_plugins/_ism/explain/...?show_policy=true")
+
+    def test_delete_policy(self):
+        self.client.index_management.delete_policy("...")
+        self.assert_url_called("DELETE", "/_plugins/_ism/policies/...")
