8694da4a87c497b4ab4556b95767be9db886815f
saimedhi <saimedhi@amazon.com>
2023-06-20 11:29:13 -0700

    Replaced botocore with aiobotocore in SigV4 asyncsigner
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- opensearchpy/helpers/asyncsigner.py
+++ opensearchpy/helpers/asyncsigner.py
@@ -33,15 +33,15 @@ class AWSV4SignerAsyncAuth:
     def __call__(self, method, url, query_string, body):  # type: ignore
         return self._sign_request(method, url, query_string, body)  # type: ignore
 
-    def _sign_request(self, method, url, query_string, body):
+    async def _sign_request(self, method, url, query_string, body):
         """
         This method helps in signing the request by injecting the required headers.
         :param prepared_request: unsigned headers
         :return: signed headers
         """
 
-        from botocore.auth import SigV4Auth
-        from botocore.awsrequest import AWSRequest
+        from aiobotocore.auth import SigV4Auth
+        from aiobotocore.awsrequest import AWSRequest
 
         # create an AWS request object and sign it using SigV4Auth
         aws_request = AWSRequest(
@@ -51,7 +51,7 @@ class AWSV4SignerAsyncAuth:
         )
 
         sig_v4_auth = SigV4Auth(self.credentials, self.service, self.region)
-        sig_v4_auth.add_auth(aws_request)
+        await sig_v4_auth.add_auth(aws_request)
         aws_request.headers["X-Amz-Content-SHA256"] = sig_v4_auth.payload(aws_request)
 
         # copy the headers from AWS request object into the prepared_request
