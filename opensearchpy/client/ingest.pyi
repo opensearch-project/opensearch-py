8945885332b858c1511117b54856a141e284ad34
saimedhi <saimedhi@amazon.com>
2023-09-28 16:05:45 -0700

    Integrated generated ingest client APIs into the existing module, ensuring alignment with the server and maintaining backward compatibility
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- opensearchpy/client/ingest.pyi
+++ opensearchpy/client/ingest.pyi
@@ -24,6 +24,15 @@
 #  specific language governing permissions and limitations
 #  under the License.
 
+# ----------------------------------------------------
+# THIS CODE IS GENERATED AND MANUAL EDITS WILL BE LOST.
+#
+# To contribute, kindly make essential modifications through either the "opensearch-py client generator":
+# https://github.com/opensearch-project/opensearch-py/blob/main/utils/generate-api.py
+# or the "OpenSearch API specification" available at:
+# https://github.com/opensearch-project/opensearch-api-specification/blob/main/OpenSearch.openapi.json
+# -----------------------------------------------------
+
 from typing import Any, Collection, MutableMapping, Optional, Tuple, Union
 
 from .utils import NamespacedClient
@@ -33,9 +42,8 @@ class IngestClient(NamespacedClient):
         self,
         *,
         id: Optional[Any] = ...,
-        master_timeout: Optional[Any] = ...,
         cluster_manager_timeout: Optional[Any] = ...,
-        summary: Optional[Any] = ...,
+        master_timeout: Optional[Any] = ...,
         pretty: Optional[bool] = ...,
         human: Optional[bool] = ...,
         error_trace: Optional[bool] = ...,
@@ -54,8 +62,8 @@ class IngestClient(NamespacedClient):
         id: Any,
         *,
         body: Any,
-        master_timeout: Optional[Any] = ...,
         cluster_manager_timeout: Optional[Any] = ...,
+        master_timeout: Optional[Any] = ...,
         timeout: Optional[Any] = ...,
         pretty: Optional[bool] = ...,
         human: Optional[bool] = ...,
@@ -74,8 +82,8 @@ class IngestClient(NamespacedClient):
         self,
         id: Any,
         *,
-        master_timeout: Optional[Any] = ...,
         cluster_manager_timeout: Optional[Any] = ...,
+        master_timeout: Optional[Any] = ...,
         timeout: Optional[Any] = ...,
         pretty: Optional[bool] = ...,
         human: Optional[bool] = ...,
@@ -110,22 +118,6 @@ class IngestClient(NamespacedClient):
         headers: Optional[MutableMapping[str, str]] = ...,
     ) -> Any: ...
     def processor_grok(
-        self,
-        *,
-        pretty: Optional[bool] = ...,
-        human: Optional[bool] = ...,
-        error_trace: Optional[bool] = ...,
-        format: Optional[str] = ...,
-        filter_path: Optional[Union[str, Collection[str]]] = ...,
-        request_timeout: Optional[Union[int, float]] = ...,
-        ignore: Optional[Union[int, Collection[int]]] = ...,
-        opaque_id: Optional[str] = ...,
-        http_auth: Optional[Union[str, Tuple[str, str]]] = ...,
-        api_key: Optional[Union[str, Tuple[str, str]]] = ...,
-        params: Optional[MutableMapping[str, Any]] = ...,
-        headers: Optional[MutableMapping[str, str]] = ...,
-    ) -> Any: ...
-    def geo_ip_stats(
         self,
         *,
         pretty: Optional[bool] = ...,
