be144479864c590956ad5b14316abf3a9cf7cf9c
dblock <dblock@amazon.com>
2023-10-09 18:10:42 -0400

    Report client-side latency.
    Signed-off-by: dblock <dblock@amazon.com>

--- samples/benchmarks/bench_sync.py
+++ samples/benchmarks/bench_sync.py
@@ -6,27 +6,35 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
+import time
 import uuid
-from threading import Thread
 from opensearchpy import OpenSearch
+from thread_with_return_value import ThreadWithReturnValue
 
 host = 'localhost'
 port = 9200
 auth = ('admin', 'admin')
 index_name = 'test-index-sync'
-item_count = 100
+item_count = 1000
 
 def index_records(client, item_count):
-    for j in range(item_count):
-        client.index(
-            index = index_name,
-                body = {
-                    'title': f"Moneyball",
-                    'director': 'Bennett Miller',
-                    'year': '2011'
-                },
-                id = uuid.uuid4()
-            )
+    data = []
+    for i in range(item_count):
+        data.append({ "index": { "_index": index_name, "_id": uuid.uuid4() }})
+        data.append({ "value": i })
+
+    start = time.time() * 1000
+    rc = client.bulk(data)
+    if rc["errors"]:
+        raise Exception(rc["errors"])
+
+    server_time = rc['took']
+    total_time = time.time() * 1000 - start
+
+    if total_time < server_time:
+        raise Exception(f"total={total_time} < server={server_time}")
+
+    return total_time - server_time
 
 def test(thread_count = 1, item_count = 1, client_count = 1):
     clients = []
@@ -43,24 +51,35 @@ def test(thread_count = 1, item_count = 1, client_count = 1):
     if clients[0].indices.exists(index_name):
         clients[0].indices.delete(index_name)
 
-    clients[0].indices.create(index_name)
+    clients[0].indices.create(index=index_name, body={
+        "mappings":{
+            "properties": {
+                "value": {
+                    "type": "float"
+                },
+            }
+        }
+    })
 
     threads = []
     for thread_id in range(thread_count):
-        thread = Thread(target=index_records, args=[
+        thread = ThreadWithReturnValue(target=index_records, args=[
             clients[thread_id % len(clients)],
             item_count
         ])
         threads.append(thread)
         thread.start()
 
+    latency = 0
     for t in threads:
-        t.join()
+        latency += t.join()
 
     clients[0].indices.refresh(index=index_name)
-    print(clients[0].count(index=index_name))
+    count = clients[0].count(index=index_name)
 
     clients[0].indices.delete(index_name)
+
+    print(f"{count}, latency={latency}")
 
 def test_1():
     test(1, 32 * item_count, item_count)
---
+++ samples/benchmarks/thread_with_return_value.py
@@ -0,0 +1,17 @@
+from threading import Thread
+
+
+class ThreadWithReturnValue(Thread):
+    
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
