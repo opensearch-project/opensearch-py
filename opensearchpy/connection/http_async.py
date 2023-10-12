e6f38ba9e607d0ed9c07478c5fd0498ecc1d0703
dblock <dblock@amazon.com>
2023-10-12 16:26:45 -0400

    Undo change in async for backwards compatibility.
    Signed-off-by: dblock <dblock@amazon.com>

--- opensearchpy/connection/http_async.py
+++ opensearchpy/connection/http_async.py
@@ -43,7 +43,7 @@ class AsyncHttpConnection(AIOHttpConnection):
         client_key=None,
         ssl_version=None,
         ssl_assert_fingerprint=None,
-        pool_maxsize=None,
+        maxsize=10,
         headers=None,
         ssl_context=None,
         http_compress=None,
@@ -140,7 +140,7 @@ class AsyncHttpConnection(AIOHttpConnection):
         self.session = None
 
         # Parameters for creating an aiohttp.ClientSession later.
-        self._limit = pool_maxsize
+        self._limit = maxsize
         self._http_auth = http_auth
         self._ssl_context = ssl_context
 
