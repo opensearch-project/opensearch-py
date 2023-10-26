fe4b6d754c2187a9ed3910b0dda9cb02948644bf
Sai Medhini Reddy Maryada <117196660+saimedhi@users.noreply.github.com>
2023-10-26 07:59:07 -0700

    added remote store client (#552)
    Signed-off-by: saimedhi <saimedhi@amazon.com>

---
+++ opensearchpy/_async/client/remote_store.pyi
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+#
+# Modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
+# ----------------------------------------------------
+# THIS CODE IS GENERATED AND MANUAL EDITS WILL BE LOST.
+#
+# To contribute, kindly make essential modifications through either the "opensearch-py client generator":
+# https://github.com/opensearch-project/opensearch-py/blob/main/utils/generate-api.py
+# or the "OpenSearch API specification" available at:
+# https://github.com/opensearch-project/opensearch-api-specification/blob/main/OpenSearch.openapi.json
+# -----------------------------------------------------
+
+from typing import Any, Collection, MutableMapping, Optional, Tuple, Union
+
+from .utils import NamespacedClient
+
+class RemoteStoreClient(NamespacedClient):
+    async def restore(
+        self,
+        *,
+        body: Any,
+        cluster_manager_timeout: Optional[Any] = ...,
+        wait_for_completion: Optional[Any] = ...,
+        pretty: Optional[bool] = ...,
+        human: Optional[bool] = ...,
+        error_trace: Optional[bool] = ...,
+        format: Optional[str] = ...,
+        filter_path: Optional[Union[str, Collection[str]]] = ...,
+        request_timeout: Optional[Union[int, float]] = ...,
+        ignore: Optional[Union[int, Collection[int]]] = ...,
+        opaque_id: Optional[str] = ...,
+        http_auth: Optional[Union[str, Tuple[str, str]]] = ...,
+        api_key: Optional[Union[str, Tuple[str, str]]] = ...,
+        params: Optional[MutableMapping[str, Any]] = ...,
+        headers: Optional[MutableMapping[str, str]] = ...,
+    ) -> Any: ...
