2db146cda2c895020febe57ea1c0995f48e57cf0
AbitraryYu <nikkoyhc@gmail.com>
2024-04-25 11:52:08 +0800

    Fix notification plugin tests, from async def to def.
    Signed-off-by: AbitraryYu <nikkoyhc@gmail.com>

--- test_opensearchpy/test_server/test_plugins/test_notification.py
+++ test_opensearchpy/test_server/test_plugins/test_notification.py
@@ -14,42 +14,51 @@ from .. import OpenSearchTestCase
 
 
 class TestNotificationPlugin(OpenSearchTestCase):
-    async def test_create_channel_notification(self) -> None:
-        content = {
-            "config_id": "sample-id",
-            "name": "sample-name",
-            "config": {
-                "name": "Sample Slack Channel",
-                "description": "This ialerting.create_destination(dummy_destination)s a Slack channel",
-                "config_type": "slack",
-                "is_enabled": True,
-                "slack": {"url": "https://sample-slack-webhook"},
-            },
-        }
-        response = self.client.plugins.notifications.create_config(content)
+    CONTENT = {
+        "config_id": "sample-id",
+        "name": "sample-name",
+        "config": {
+            "name": "Sample Slack Channel",
+            "description": "This ialerting.create_destination(dummy_destination)s a Slack channel",
+            "config_type": "slack",
+            "is_enabled": True,
+            "slack": {"url": "https://sample-slack-webhook"},
+        },
+    }
+
+    def test_create_channel_notification(self) -> None:
+        response = self.client.plugins.notifications.create_config(self.CONTENT)
 
         self.assertNotIn("errors", response)
         self.assertIn("config_id", response)
 
-    async def test_list_all_channel_configurations(self) -> None:
+    def test_list_all_channel_configurations(self) -> None:
+        self.test_create_channel_notification()
+
         response = self.client.plugins.notifications.list_features()
 
         self.assertNotIn("errors", response)
         self.assertIn("config_id", response)
 
-    async def test_list_all_notification_configurations(self) -> None:
+    def test_list_all_notification_configurations(self) -> None:
+        self.test_create_channel_notification()
+
         response = self.client.plugins.notifications.get_config()
 
         self.assertNotIn("errors", response)
         self.assertIn("config_id", response)
 
-    async def test_get_channel_configuration(self) -> None:
+    def test_get_channel_configuration(self) -> None:
+        self.test_create_channel_notification()
+
         response = self.client.plugins.notifications.get_config(config_id="sample-id")
 
         self.assertNotIn("errors", response)
         self.assertIn("config_id", response)
 
-    async def test_update_channel_configuration(self) -> None:
+    def test_update_channel_configuration(self) -> None:
+        self.test_create_channel_notification()
+
         response = self.client.plugins.notifications.update_config(
             config_id="sample-id"
         )
@@ -57,8 +66,9 @@ class TestNotificationPlugin(OpenSearchTestCase):
         self.assertNotIn("errors", response)
         self.assertIn("config_id", response)
 
-    async def test_delete_channel_configuration(self) -> None:
-        # Try fetching the destination
+    def test_delete_channel_configuration(self) -> None:
+        self.test_create_channel_notification()
+
         response = self.client.plugins.notifications.delete_config(
             config_id="sample-id"
         )
